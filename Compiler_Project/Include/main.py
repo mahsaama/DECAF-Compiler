71453cc849fc7146f205367182b59f8b83044196
R Dehghan <rdb1377@gmail.com>
2020-10-20 12:50:09 +0330

    import lexer

--- Compiler_Project/Include/main.py
+++ Compiler_Project/Include/main.py
@@ -1,15 +1,18 @@
 import sys, getopt
+from DecafLexer import *
+
+
 def main(argv):
     inputfile = ''
     outputfile = ''
     try:
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
     except getopt.GetoptError:
-        print ('main.py -i <inputfile> -o <outputfile>')
+        print('main.py -i <inputfile> -o <outputfile>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print ('test.py -i <inputfile> -o <outputfile>')
+            print('test.py -i <inputfile> -o <outputfile>')
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
@@ -17,22 +20,20 @@ def main(argv):
             outputfile = arg
 
     with open("tests/" + inputfile, "r") as input_file:
-        # do stuff with input file
-        pass
+    # do stuff with input file
+        lexer.input(input_file.read())
 
     with open("out/" + outputfile, "w") as output_file:
-        # write result to output file. 
+        # write result to output file.
         # for the sake of testing :
-        output_file.write("""class
-T_ID Program
-{
-void
-T_ID main
-(
-)
-{
-}
-}""")
+        # output_file.write()
+        sys.stdout = output_file
+        for token in get_token(lexer):
+            if token.type[:2] == 'T_':
+                print("%s %s" % (token.type, token.value))
+
+            else:
+                print(token.value)
 
 
 if __name__ == "__main__":
