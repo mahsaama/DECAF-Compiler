50324633f6ac6fb949971d29eb6dc2f63fd07d93
R Dehghan <rdb1377@gmail.com>
2020-10-21 07:53:15 -0700

    Update main.py

--- Compiler_Project/Include/main.py
+++ Compiler_Project/Include/main.py
@@ -1,18 +1,17 @@
 import sys, getopt
-from DecafLexer import *
-
+from DecafLexerREGEX import *
 
 def main(argv):
     inputfile = ''
     outputfile = ''
     try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
     except getopt.GetoptError:
-        print('main.py -i <inputfile> -o <outputfile>')
+        print ('main.py -i <inputfile> -o <outputfile>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('test.py -i <inputfile> -o <outputfile>')
+            print ('test.py -i <inputfile> -o <outputfile>')
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
@@ -20,21 +19,18 @@ def main(argv):
             outputfile = arg
 
     with open("tests/" + inputfile, "r") as input_file:
-    # do stuff with input file
-        lexer.input(input_file.read())
-
+        myLexer = Lexer(tokens)  
+        myLexer.input(input_file.read())
+    	
     with open("out/" + outputfile, "w") as output_file:
-        # write result to output file.
-        # for the sake of testing :
-        # output_file.write()
-        sys.stdout = output_file
-        for token in get_token(lexer):
-            if token.type[:2] == 'T_':
-                print("%s %s" % (token.type, token.value))
+        sys.stdout =  output_file      
+        try:
+                for token in myLexer.tokenizer():
+                    print(token)
+        except LexerError:
+                print('UNDEFINED_TOKEN')
 
-            else:
-                print(token.value)
-
+			
 
 if __name__ == "__main__":
     main(sys.argv[1:])
